692c5c209cff1dfc656d5c33df657fd3a1b71ce3
Piyush Dhimate <piyush.dhimate@rapidcircle.com>
2020-11-16 12:10:51 +1100

    Added PS script

---
+++ face_mask/dataset/CopyToSingleFolder.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$Src = "C:\Work\GitHub\machinelearning2\face_mask\dataset\raw"
+$Dst = "C:\Work\GitHub\machinelearning2\face_mask\dataset\raw\NewImages"
+
+$count = 0
+
+foreach($file in Get-ChildItem $Src -Recurse -File)
+{
+    $guid = New-Guid
+    $newName = Join-Path $Dst ($guid.ToString() + $file.Extension)
+   
+    Copy-Item -Path $file.PSPath -Destination $newName 
+    $count++
+}
+Write-Output "Copied $count files"
+
